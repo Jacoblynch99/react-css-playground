62f68f002947692cb1b0053017742ad08f1eddcc
Jacoblynch99 <jacoblynch99@gmail.com>
2021-02-03 14:56:09 -0600

    initial commit

--- src/components/UserLanding.js
+++ src/components/UserLanding.js
@@ -13,11 +13,99 @@ import {
     DialogTitle,
     DialogActions,
     Container,
+    Grid,
 } from '@material-ui/core'
 import React, { useState } from 'react'
 
 const UserLanding = (props) => {
-    return <h1>Test</h1>
+    return (
+        <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+            style={{ width: '100vw', height: '100vh' }}
+        >
+            <Grid
+                item
+                xs={3}
+                container
+                direction="column"
+                style={{ width: '25vw' }}
+            >
+                <Grid
+                    item
+                    style={{ height: '40vh', borderStyle: 'solid', margin: 5 }}
+                >
+                    1
+                </Grid>
+                <Grid
+                    item
+                    style={{ height: '40vh', borderStyle: 'solid', margin: 5 }}
+                >
+                    5
+                </Grid>
+                <Grid
+                    item
+                    style={{ height: '40vh', borderStyle: 'solid', margin: 5 }}
+                >
+                    9
+                </Grid>
+            </Grid>
+            <Grid
+                item
+                xs={3}
+                container
+                direction="column"
+                style={{ width: '25vw' }}
+            >
+                <Grid
+                    item
+                    style={{ height: '40vh', borderStyle: 'solid', margin: 5 }}
+                >
+                    2
+                </Grid>
+                <Grid
+                    item
+                    style={{ height: '40vh', borderStyle: 'solid', margin: 5 }}
+                >
+                    6
+                </Grid>
+                <Grid
+                    item
+                    style={{ height: '40vh', borderStyle: 'solid', margin: 5 }}
+                >
+                    10
+                </Grid>
+            </Grid>
+            <Grid
+                item
+                xs={3}
+                container
+                direction="column"
+                style={{ width: '25vw' }}
+            >
+                <Grid
+                    item
+                    style={{ height: '40vh', borderStyle: 'solid', margin: 5 }}
+                >
+                    3
+                </Grid>
+                <Grid
+                    item
+                    style={{ height: '40vh', borderStyle: 'solid', margin: 5 }}
+                >
+                    7
+                </Grid>
+                <Grid
+                    item
+                    style={{ height: '40vh', borderStyle: 'solid', margin: 5 }}
+                >
+                    11
+                </Grid>
+            </Grid>
+        </Grid>
+    )
 }
 
 export default UserLanding
